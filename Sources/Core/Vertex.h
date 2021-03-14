e4bb75ac59b6159bc8e029b59ee758c5a18cf0ec
SeolYang <kyowonhorang@gmail.com>
2021-03-15 00:19:33 +0900

    Implement Vertex

--- Sources/Core/Vertex.h
+++ Sources/Core/Vertex.h
@@ -1,2 +1,16 @@
 #pragma once
 #include <Core/CoreMinimal.h>
+
+namespace sr
+{
+   struct Vertex
+   {
+   public:
+      glm::vec4 Position;
+      glm::vec2 TexCoord;
+      glm::vec3 Normal;
+      glm::vec4 Tangent;
+      glm::vec4 BiTangent;
+
+   };
+}
