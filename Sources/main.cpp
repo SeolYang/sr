8a2427b7bca655307b7b42f960598a1d73f82b76
SeolYang <kyowonhorang@gmail.com>
2021-03-14 13:48:51 +0900

    Initialize project

---
+++ Sources/main.cpp
@@ -0,0 +1,10 @@
+#include <json/json.hpp>
+#include <tinygltf/tiny_gltf.h>
+#include <icecream/icecream.hpp>
+#include <glm/glm.hpp>
+#include <stb_image/stb_image.h>
+
+int main()
+{
+   return 0;
+}
